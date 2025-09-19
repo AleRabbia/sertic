01dbc0ff81adea6f10d338f88d48376dd1d84171
AleRabbia <alexisrabbia@gmail.com>
2025-09-19 09:38:43 -0300

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import TestimoniosPage from './pages/TestimoniosPage';
 import CasosExitoPage from './pages/CasosExitoPage';
