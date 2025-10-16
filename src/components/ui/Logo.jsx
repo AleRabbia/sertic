5acd60ffe2875c8032f11766e2f2a24798961383
AleRabbia <alexisrabbia@gmail.com>
2025-10-16 12:59:11 -0300

    logo vectirizados

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,4 +1,5 @@
 import logo2a from '../../assets/logo2a.png'
+import logo_blanco from '../../assets/logo_blanco.png'
 
 export const Logo = ({ size = 'md' }) => {
   const sizes = {
@@ -10,8 +11,17 @@ export const Logo = ({ size = 'md' }) => {
   return (
     <div className="flex items-center space-x-2">
       <div className={`${sizes[size]} flex items-center justify-center`}>
+        { /* si queremos poner el mismo formato que el boton del chatbot
+        cambiamos la clase de arriba por la siguiente
+        
+        <div className={`${sizes[size]} flex items-center justify-center 
+                      bg-gradient-to-r from-blue-600 to-purple-600 p-1 rounded-xl`}>
+
+        */}
+
+
         <img 
-          src={logo2a}
+          src={logo_blanco}
           alt="SerTIC Tech Solutions" 
           className="w-full h-full object-contain rounded-xl"
         />
